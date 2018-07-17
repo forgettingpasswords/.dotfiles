24a6c6a039197f9c477a11e73ab31f9602565472
MyOde <zod@tfwno.gf>
2018-07-17 10:18:55 +0200

    Changed shell back to fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+# emacs ansi-term support
+if test -n "$EMACS"
+    set -x TERM eterm-color
+end
+
+# this function may be required
+function fish_title
+    true
+end
+
+set -gx PATH ~/.local/bin $PATH
+setxkbmap -option grp:switch,grp:win_space_toggle,grp_led:scroll us,lv -variant alt
